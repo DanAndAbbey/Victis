757e5dd701eb43dce8146dcf64a0b9ec23f23ea1
danandabbey <danandabbey502@gmail.com>
2023-03-16 21:37:37 -0400

    working on calculator

--- Scripts/finance.js
+++ Scripts/finance.js
@@ -1,132 +1,107 @@
 let app = {
     list:[],
-};
-
-let selectedButton = document.querySelectorAll("button");
-selectedButton.forEach(function(button){
-    button.addEventListener("click",function(e){
-        e.preventDefault();
-        let button = e.currentTarget;
-
-        document.getElementById("intro").style.display="none";
-        document.getElementById("form").style.display="flex";
-
-        let action = button.id
-
-        if (action === "one")
-            listen()
-
-
-        else if (action === "multiple"){
-            document.getElementById("done").style.display="flex";
-            listen()
-        }
-
-    });
-});
-
-//intro
-
-
-
-    function listen(){
-
+    listen: function(){
         let selectedButton = document.querySelectorAll("button");
         selectedButton.forEach(function(button){
             button.addEventListener("click",function(e){
                 e.preventDefault();
-                let button = e.currentTarget;
-
-                let action = button.id
-
-                if (action === "done")
-                    repeat();
-        
-                else if (action === "submit"){
-                    document.getElementById("done").style.display="flex";
-                    submit();
-                }
-
+                button = e.currentTarget;
+    
+                let a= button.id
+                action(a);    
             });
         });
+    }
+};
 
+app.listen();
+
+//intro
+
+
+function action(a){
+
+    if (a === "one"){
+        document.getElementById("intro").style.display="none";
+        document.getElementById("form").style.display="flex";
     }
 
 
+    else if (a === "multiple"){
 
+        document.getElementById("intro").style.display="none";
+        document.getElementById("form").style.display="flex";
+        document.getElementById("done").style.display="flex";
+    }
 
-    function repeat(){
+    else if (a === "done"){
         readStock();
-        listen();  
+    }
+
+    else if (a === "submit"){
+        document.getElementById("done").style.display="flex";
+        readStock();
+        print(); 
+
+    };
+};
+
+
+function readStock(){
+    let stock = {
+        symbol: document.getElementById("symbol").value,
+        price: document.getElementById("price").value,
+        quantity: document.getElementById("quantity").value,
+        yield: document.getElementById("yield").value,
+        years: document.getElementById("years").value,
+        frequency: document.getElementById("frequency").value
     };
 
-    function submit(){
-        readStock();
-        print();
-
-    };
-
-
-
-    function readStock(){
-        let stock = {
-            symbol: document.getElementById("symbol").value,
-            price: document.getElementById("price").value,
-            quantity: document.getElementById("quantity").value,
-            yield: document.getElementById("yield").value,
-            years: document.getElementById("years").value,
-            frequency: document.getElementById("frequency").value
-        };
-
-        function disFrequency(freq){
-            if (freq === "monthly"){
-                return 12
-            }
-            else if (freq === "quarterly"){
-                return 4
-            }
-            else if (freq === "yearly"){
-                return 1
-            }
-        };
-
-        function findFinalValue(value,yield,distributions){
-            let newValue = value
-            for (let i = 0; i < distributions; i++){
-            newValue = (yield * newValue) + newValue
-            };
-            return newValue
+    function disFrequency(freq){
+        if (freq === "monthly"){
+            return 12
         }
-        
-        let ticker = (stock.symbol)
-        let years = (stock.years)
-        let yield = (parseFloat(stock.yield) * .01)/disFrequency(stock.frequency);
-        let distributions = parseInt(stock.years * (disFrequency(stock.frequency)));
-        let cost = parseFloat(stock.price * stock.quantity);
-        let value = cost;
+        else if (freq === "quarterly"){
+            return 4
+        }
+        else if (freq === "yearly"){
+            return 1
+        }
+    };
 
-        let finalValue = findFinalValue(value,yield,distributions);
+    function findFinalValue(value,yield,distributions){
+        let newValue = value
+        for (let i = 0; i < distributions; i++){
+        newValue = (yield * newValue) + newValue
+        };
+        return newValue
+    }
 
-        
+    let yield = (parseFloat(stock.yield) * .01)/disFrequency(stock.frequency);
+    let distributions = parseInt(stock.years * (disFrequency(stock.frequency)));
+    let finalValue = findFinalValue(parseFloat(stock.price * stock.quantity),yield,distributions);
 
-        (app.list).push([ticker,finalValue,years]);
+    
+
+    (app.list).push([stock.symbol,finalValue,stock.years]);
 };
 
 
 //print results
 
-    function print(){
-        const end = document.getElementById("result");
-        document.getElementById("intro").style.display="none";
-        document.getElementById("form").style.display="none";
-        document.getElementById("end").style.display="flex";
-        
-        app.list.forEach(function(stock){
-            ticker =stock[0],
-            finalValue = stock[1],
-            years = stock[2]
+function print(){
+    const end = document.getElementById("result");
+    document.getElementById("intro").style.display="none";
+    document.getElementById("form").style.display="none";
+    document.getElementById("end").style.display="flex";
+    
+    app.list.forEach(function(stock){
+        ticker =stock[0];
+        finalValue = stock[1];
+        years = stock[2];
 
-        let message = `Your shares of ${ticker.toUpperCase()} will be worth $${finalValue.toFixed(2)} in ${years} years.`;
+        let newLine = "\n"
+        let message = (`Your shares of ${ticker.toUpperCase()} will be worth $${finalValue.toFixed(2)} in ${years} years.`);
         end.append(message);
-        });
-    };
+    });
+};
